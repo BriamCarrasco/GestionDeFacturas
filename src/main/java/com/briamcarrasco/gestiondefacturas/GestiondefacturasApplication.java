9de550d968c7cec0f80ceccbe007599409816024
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-03-29 22:19:59 -0300

    archivos iniciales

---
+++ src/main/java/com/briamcarrasco/gestiondefacturas/GestiondefacturasApplication.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.gestiondefacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondefacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondefacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briamcarrasco/gestiondefacturas/GestiondefacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.gestiondefacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondefacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
